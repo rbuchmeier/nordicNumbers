d2b5e59022c6f71553756819d88a89f90409f039
Ryan Buchmeier <rwbleo@gmail.com>
2017-05-03 18:21:08 -0400

    Basic parsing of filename to get race details
    Add Race to DB has a function designed to get
    race details from the filename, then identify
    get any missing information from the user.
    
    This still needs refactored and tested, but it is a start.

--- src/scripts/add_race_to_db.py
+++ src/scripts/add_race_to_db.py
@@ -1,13 +1,52 @@
-POSSIBLE_HEADERS = {
-    "gender": ["gender", "sex"],
-    "last": ["last", "last name"],
-    "first": ["first", "first"],
-    "team": ["team"],
-    "start": ["start", "start time"],
-    "finish": ["finish", "finish time"],
-    "time": ["total time", "time"],
-    "grade": ["grade", "class"]
-}
+from utils import LOCATIONS
+import re
+
+def get_file(filename):
+    pass
+
+def get_race_info(filename):
+    details = filename.split('_')
+    race_details = {}
+    # Location
+    for location in LOCATIONS:
+        if location in details:
+            race_details['location'] = location
+    # School Level
+    if 'ms' in details:
+        race_details['level'] = 'ms'
+    elif 'hs' in details:
+        race_details['level'] = 'hs'
+    # Distance
+    dist_re = re.compile('[0-9]+k')
+    dist_matches = filter(dist_re.match, details)
+    if len(dist_matches) > 0:
+        race_details['distance'] = dist_matches.group()
+    # Date
+    dist_re = re.compile('[0-9]{4}-[0-9]{2}-[0-9]{2}')
+    dist_matches = filter(dist_re.match, details)
+    if len(dist_matches) > 0:
+        race_details['date'] = dist_matches.group()
+    # Gender
+    if 'boys' in details:
+        race_details['gender'] = 'Boys'
+    elif 'girls' in details:
+        race_details['gender'] = 'Girls'
+    # Skill
+    if 'skate' in details:
+        race_details['skill'] = 'Skate'
+    elif 'classic' in details:
+        race_details['skill'] = 'Classic'
+
+    return race_details
+
+def parse_file(race_file):
+    pass
+
+def get_skier_info(skier, headers):
+    pass
+
+def write_race_to_db(race):
+    pass
 
 def main():
     # Parse data
@@ -16,7 +55,7 @@ def main():
     # Enter information into db
     filename = input('Please enter a filename: ')
     f = get_file(filename)
-    race = get_race_info()
+    race = get_race_info(filename)
     headers = get_headers(f)
     race = parse_file(f)
     for skier in data:
--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -1,3 +1,5 @@
+LOCATIONS = ['Casper', 'Laramie', 'Cody', 'Jackson', 'Lander', 'Pinedale']
+
 def parse_time_string_to_number_seconds(str_time):
     """ takes in a string (with seconds being after the last ':') and converts it into a number of seconds. """
     pass
