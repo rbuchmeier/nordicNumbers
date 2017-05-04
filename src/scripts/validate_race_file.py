2890591baf59d0004ebf7efde2a112d4429a5002
Ryan Buchmeier <rwbleo@gmail.com>
2017-05-04 15:43:56 -0600

    Use data structure instead of file handler

--- src/scripts/validate_race_file.py
+++ src/scripts/validate_race_file.py
@@ -1,23 +1,77 @@
 import csv
+from utils import LOCATIONS
+REQUIRED_FIELDS = ['location', 'date', 'gender']
 
 def get_file(filename):
-    ifile = open(filename, 'r')
-    return ifile
+    with open(filename) as f:
+        content = f.readlines()
+    return [data.strip() for data in content]
 
-def write_validated_race(f, details):
-    ignore_columns = get_columns_to_ignore(f)
-    new_header_names = get_better_header_names(f)
-    write_race(f, ignore_columns, new_header_names)
+def write_validated_race(data, details, headers_to_avoid):
+    if should_split_genders():
+        write_race(data, details, headers_to_avoid, 'Boys')
+        write_race(data, details, headers_to_avoid, 'Girls')
+    else:
+        write_race(data, details, headers_to_avoid)
+
+def write_race(data, details, bad_headers):
+    pass
+
+def get_new_filename(filename):
+    new_filename = ''
+    new_filename += get_level(filename)
+    new_filename += get_location(filename)
+    new_filename += get_skill(filename)
+    new_filename += get_distance(filename)
+    new_filename += get_date(filename)
+    new_filename = new_filename.rstrip('_') + '.csv'
+    return new_filename
+
+def get_level(filename):
+    if 'junior high' in filename.lower():
+        return 'ms_'
+    else:
+        level = input('High School (hs) or Middle School (ms): ')
+        assert (level.lower() in ['hs', 'ms'])
+        return level.lower() + '_'
+
+def get_location(filename):
+    for location in LOCATIONS:
+        if location.lower() in filename.lower():
+            return location.lower() + '_'
+    location = input('Where did this take place? (eg casper, lander, etc...)')
+    assert(location in LOCATIONS)
+    return location.lower() + '_'
+
+def get_skill(filename):
+    if 'skate' in filename.lower():
+        return 'skate_'
+    elif 'classic' in filename.lower():
+        return 'classic_'
+    else:
+        skill = input('What skill type? (skate or classic?): ')
+        assert(skill.lower() in ['skate', 'classic'])
+        return skill.lower() + '_'
+
+def get_distance(filename):
+    distance = input('What distance? (5k, 10k, etc...): ')
+    return distance.lower() + '_'
+
+def get_date(filename):
+    date = input('What date? (e.g. 2017-02-18): ')
+    return date.lower() + '_'
+    
 
 def main():
     # ask for filename
-    # try to figure out what race it goes to
-    # prompt for confirmation and to fill in blanks
-    # Try to 
+    # Must make filename have location, raceDate, gender, skill, distance to best possible
+    # Make headers match first, last, team, start, finish, time, grade
+    # Split up file into Boys and Girls individual files
     filename = input('Please enter a filename: ')
-    f = get_file(filename)
-    race_details = get_race_details(f)
-    write_validated_race(f, race_details)
+    data = get_file(filename)
+    race_details = get_new_filename(filename)
+    useless_header_indexes = get_useless_header_indexes(data)
+    write_validated_race(data, race_details, useless_header_indexes)
 
 if __name__ == "__main__":
     main()
