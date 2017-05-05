23f4c28141cc9acc11f98e1a3394f8b7cea662a5
Ryan Buchmeier <rwbleo@gmail.com>
2017-05-05 10:16:47 -0600

    Adds basic testing for validate_race_file script

--- src/scripts/test_validate_race_file.py
+++ src/scripts/test_validate_race_file.py
@@ -1,10 +1,11 @@
-from validate_race_file import get_file
 from validate_race_file import write_validated_race
 from validate_race_file import get_level
 from validate_race_file import get_location
 from validate_race_file import get_skill
 from validate_race_file import get_distance
 from validate_race_file import get_date
+# from validate_race_file import remove_useless_columns
+from validate_race_file import get_useless_header_indexes
 
 import unittest
 from mock import Mock
@@ -46,6 +47,37 @@ class ValidateRaceTests(unittest.TestCase):
         expected = '2017-02-18_'
         actual = get_date(test_filename)
         self.assertEqual(expected, actual)
+
+    def test_get_useless_header_indexes(self):
+        test_data = [['first', 'last', 'foo', 'time'],
+                     ['Ryan', 'Bucky', 1, '11:50'],
+                     ['BrenN', 'Azure', 2, '9:00']]
+        expected = [2]
+        actual = get_useless_header_indexes(test_data)
+        self.assertEqual(expected, actual)
+
+    @unittest.skip('finish function, then unskip')
+    def test_remove_useless_columns(self):
+        test_data = [['first', 'last', 'foo', 'time'],
+                     ['Ryan', 'Bucky', 1, '11:50'],
+                     ['BrenN', 'Azure', 2, '9:00']]
+        expected = [['first', 'last', 'time'],
+                     ['Ryan', 'Bucky', '11:50'],
+                     ['BrenN', 'Azure', '9:00']]
+        actual = remove_useless_columns(test_data)
+        self.assertEqual(expected, actual)
+
+    @unittest.skip('finish function, then unskip')
+    def test_remove_useless_columns_with_gender(self):
+        test_data = [['first', 'last', 'foo', 'time', 'gender'],
+                     ['Ryan', 'Bucky', 1, '11:50', 'Boys'],
+                     ['BrenN', 'Azure', 2, '9:00', 'M']]
+        expected = [['first', 'last', 'time', 'gender'],
+                     ['Ryan', 'Bucky', '11:50', 'Boys'],
+                     ['BrenN', 'Azure', '9:00', 'M']]
+        actual = remove_useless_columns(test_data)
+        self.assertEqual(expected, actual)
+
 
 if __name__ == '__main__':
     unittest.main()
