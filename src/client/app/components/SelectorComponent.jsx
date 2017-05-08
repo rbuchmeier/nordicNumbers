03da64f895901462a8ae5cae4e7a5d3928726821
gannetjg <denalijosh2@gmail.com>
2017-05-08 17:22:51 -0600

    Added radio buttons "Casper 2017", and "Laramie 2017"

--- src/client/app/components/SelectorComponent.jsx
+++ src/client/app/components/SelectorComponent.jsx
@@ -5,6 +5,10 @@ class SelectorComponent extends React.Component {
     return (
       <div id='selector-component'>
         <p>Race Selector</p>
+          <label>
+           <input type="radio" name="Casper 2017">Casper 2017</input>
+           <input type="radio" name="Laramie 2017">Laramie 2017</input>
+          </label>
       </div>
     );
   }
