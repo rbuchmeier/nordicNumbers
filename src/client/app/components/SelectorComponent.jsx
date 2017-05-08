b7589b382202c7f614aae6df6d271dfa2d7f48d2
Ryan Buchmeier <rwbleo@gmail.com>
2017-05-08 19:40:06 -0400

    Fix Radio Buttons so they are viewable

--- src/client/app/components/SelectorComponent.jsx
+++ src/client/app/components/SelectorComponent.jsx
@@ -5,10 +5,10 @@ class SelectorComponent extends React.Component {
     return (
       <div id='selector-component'>
         <p>Race Selector</p>
-          <label>
-           <input type="radio" name="Casper 2017">Casper 2017</input>
-           <input type="radio" name="Laramie 2017">Laramie 2017</input>
-          </label>
+        <form>
+           <input type="radio" name="Casper_2017" />Casper 2017<br/>
+           <input type="radio" name="Laramie_2017" />Laramie 2017
+        </form>
       </div>
     );
   }
