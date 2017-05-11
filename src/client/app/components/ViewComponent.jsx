ed19332ef595466e4834f236679f1f1b4bd29124
Ryan Buchmeier <rwbleo@gmail.com>
2017-05-11 17:22:15 -0400

    Remove RacerComponent (just use ViewComponent)

--- src/client/app/components/ViewComponent.jsx
+++ src/client/app/components/ViewComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 var axios = require('axios');
-import RacerComponent from './RacerComponent.jsx';
+var moment = require('moment');
 
 var ViewComponent = React.createClass({
   getInitialState: function() {
@@ -28,7 +28,17 @@ var ViewComponent = React.createClass({
         <h3>Race View</h3>
         <p>location: {this.state.races.location}</p>
         <p>skill: {this.state.races.skill}</p>
-        <RacerComponent racers={this.state.racers}/>
+        <ul>
+            {this.state.racers.map(function(racer) {
+                return (
+                    <li key={racer.first + racer.last + racer.time}>
+                        {racer.first} {racer.last} ({racer.grade}) ---&nbsp;
+                        {moment('2017-01-01').startOf('day').seconds(racer.time).format('H:mm:ss')} |&nbsp;
+                        {racer.team}
+                    </li>
+                );
+            })}
+        </ul>
       </div>
     );
   }
