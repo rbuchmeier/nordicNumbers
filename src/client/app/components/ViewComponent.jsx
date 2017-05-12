d813cf3678ea536e13f950f8427ea9fa7ccb2420
Ryan Buchmeier <rwbleo@gmail.com>
2017-05-12 12:38:11 -0400

    Hoist state to App Component
    The View componenet (which is a Presentational
    Component) should probably not control
    the state. Rather, that should be controlled
    by our index.jsx file (App Component, which is
    the Container Component).

--- src/client/app/components/ViewComponent.jsx
+++ src/client/app/components/ViewComponent.jsx
@@ -3,33 +3,15 @@ var axios = require('axios');
 var moment = require('moment');
 
 var ViewComponent = React.createClass({
-  getInitialState: function() {
-    return {
-        races: [],
-        racers: []
-    }
-  },
-  componentDidMount: function() {
-    var _this = this;
-    this.serverRequest = axios.get('http://192.81.218.23:3000/api/races')
-        .then(function(result) {
-            _this.setState({
-                races: result.data[0],
-                racers: result.data[0].racers
-            });
-        })
-  },
-  componentWillUnmount: function() {
-    this.serverRequest.abort();
-  },
   render: function() {
+    console.log(this.props);
     return (
       <div id='view-component'>
         <h3>Race View</h3>
-        <p>location: {this.state.races.location}</p>
-        <p>skill: {this.state.races.skill}</p>
+        <p>location: {this.props.races.location}</p>
+        <p>skill: {this.props.races.skill}</p>
         <ul>
-            {this.state.racers.map(function(racer) {
+            {this.props.races.racers.map(function(racer) {
                 return (
                     <li key={racer.first + racer.last + racer.time}>
                         {racer.first} {racer.last} ({racer.grade}) ---&nbsp;
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
+var axios = require('axios');
+var moment = require('moment');
 import {render} from 'react-dom';
 import FilterComponent from './components/FilterComponent.jsx'
 import SelectorComponent from './components/SelectorComponent.jsx'
 import ViewComponent from './components/ViewComponent.jsx'
 
-class App extends React.Component {
-  render () {
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+        races: {racers: []}
+    }
+  },
+  componentDidMount: function() {
+    var _this = this;
+    this.serverRequest = axios.get('http://192.81.218.23:3000/api/races')
+        .then(function(result) {
+            _this.setState({
+                races: result.data[0]
+            });
+        })
+  },
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
+  render: function() {
+    console.log(this.state.races);
     return (
 	<div>
       <div className='jumbotron'>
         <FilterComponent />
         <SelectorComponent />
-        <ViewComponent />
+        <ViewComponent races={this.state.races}/>
       </div>
 	</div>
     );
   }
-}
+})
 
 
 render(<App/>, document.getElementById('app'));
