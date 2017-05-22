5b644b798edbe28f9bd58c0064daa814500b1b3a
gannetjg <denalijosh2@gmail.com>
2017-05-22 17:46:04 -0600

    Added drop-down menues for the race filter.

--- src/client/app/components/FilterComponent.jsx
+++ src/client/app/components/FilterComponent.jsx
@@ -6,7 +6,30 @@ class FilterComponent extends React.Component {
     return (
       <div id='filter-component'>
         <h3>Race Filter</h3>
-      </div>
+      <div class="row">
+	<div class"col-md-4">
+		Gender:&nbsp
+		<select name="genders">
+			<option value="boys">Boys</option>
+			<option value="girls">Girls</option>
+		</select>
+	</div>
+	<div class"col-md-4">
+		Skill:&nbsp
+		<select name="skill">
+			<option value="skate">Skate</option>
+			<option value="classic">Classic</option>
+		</select>
+	</div>
+	<div class"col-md-4">
+		Season:&nbsp
+		<select name="season">
+			<option value="16_17">16/17</option>
+			<option value="15_16">15/16</option>
+		</select>
+	</div>
+	</div>
+	</div>
     );
   }
 
