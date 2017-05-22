68ccc45ae059b8b07be763aa26eb950be48f7602
gannetjg <denalijosh2@gmail.com>
2017-05-22 17:00:47 -0600

    Used bootstrap to add columns for race results.

--- src/client/app/components/ViewComponent.jsx
+++ src/client/app/components/ViewComponent.jsx
@@ -6,21 +6,22 @@ var ViewComponent = React.createClass({
     return (
       <div id='view-component'>
         <h3>Race View</h3>
+<div>
         <p>location: {this.props.race.location}</p>
         <p>skill: {this.props.race.skill}</p>
         <p>ID: {this.props.race._id}</p>
-        <ul>
-            <li key="label"><em>Name (Grade) --- Time | Team</em></li>
+</div>
+      <div class="row" key={"racers"+this.props.race._id}><em>Name (Grade) --- Time | Team</em></div>
             {this.props.race.racers.map(function(racer) {
                 return (
-                    <li key={racer.first + racer.last + racer.time}>
-                        {racer.first} {racer.last} ({racer.grade}) ---&nbsp;
-                        {moment('2017-01-01').startOf('day').seconds(racer.time).format('H:mm:ss')} |&nbsp;
-                        {racer.team}
-                    </li>
+                    <div class="row" key={racer.first + racer.last + racer.time}>
+                   <div class="col-md-3"> {racer.first} {racer.last}</div>
+		   <div class="col-md-3">{racer.grade}</div>
+                   <div class="col-md-3">{moment('2017-01-01').startOf('day').seconds(racer.time).format('H:mm:ss')}</div>
+                   <div class="col-md-3'>{racer.team}</div>
+                    </div>
                 );
             })}
-        </ul>
       </div>
     );
   }
