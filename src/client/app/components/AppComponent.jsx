25da648cf88c97ada5f41dea2cdff09ddd0fd2a1
Ryan Buchmeier <rwbleo@gmail.com>
2017-05-24 13:26:44 -0400

    Refactor out App component from Redux Provider
    Redux has it's own was of passing everything
    around, and best practice requires everything
    to be wrapped in a 'Provider' Component.
    
    Which works, because then App can be our
    Container Component and live next to
    all of the other components...or maybe it should
    just be a Presentatinal component? Hmm....

---
+++ src/client/app/components/AppComponent.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import axios from 'axios';
+import FilterComponent from './FilterComponent.jsx'
+import SelectorComponent from './SelectorComponent.jsx'
+import ViewComponent from './ViewComponent.jsx'
+
+var App = React.createClass({
+  componentDidMount: function() {
+    var _this = this;
+    this.serverRequest = axios.get('http://192.81.218.23:3000/api/races')
+        .then(function(result) {
+            _this.props.loadRaces({type: 'load_races', payload: result.data});
+            _this.props.changeRace({type: 'change_race', payload: result.data[0]});
+        })
+  },
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
+  render: function() {
+    return (
+    <div>
+      <div className='jumbotron'>
+        <FilterComponent loadRaces={this.props.loadRaces}/>
+        <SelectorComponent races={this.props.races} changeRace={this.props.changeRace}/>
+        <ViewComponent race={this.props.race} />
+      </div>
+    </div>
+    );
+  }
+})
+
+export default App;
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,38 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-var axios = require('axios');
-var moment = require('moment');
 import {render} from 'react-dom';
 import { createStore } from 'redux';
 import { Provider, connect } from 'react-redux';
-import FilterComponent from './components/FilterComponent.jsx'
-import SelectorComponent from './components/SelectorComponent.jsx'
-import ViewComponent from './components/ViewComponent.jsx'
-
-var App = React.createClass({
-  componentDidMount: function() {
-    var _this = this;
-    this.serverRequest = axios.get('http://192.81.218.23:3000/api/races')
-        .then(function(result) {
-            _this.props.loadRaces({type: 'load_races', payload: result.data});
-            _this.props.changeRace({type: 'change_race', payload: result.data[0]});
-        })
-  },
-  componentWillUnmount: function() {
-    this.serverRequest.abort();
-  },
-  render: function() {
-    return (
-	<div>
-      <div className='jumbotron'>
-        <FilterComponent loadRaces={this.props.loadRaces}/>
-        <SelectorComponent races={this.props.races} changeRace={this.props.changeRace}/>
-        <ViewComponent race={this.props.race} />
-      </div>
-	</div>
-    );
-  }
-})
+import App from './components/AppComponent.jsx';
 
 var initialState = {
     data: {
