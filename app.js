f648f39a2a308543c1d639917790f84f7f2880eb
Ryan Buchmeier <rwbleo@gmail.com>
2017-05-26 22:40:07 -0400

    All CORS to server to allow for local development

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ var app = express();
 var router = express.Router();
 
 var Race = require('./src/client/app/models/race');
-
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 router.get('/', function(req, res) {
     res.json({message: 'hoorway! welcome to our api!'});
 });
