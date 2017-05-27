f13104424f62bb99d7f1d9bce7bf59e64b97bc60
Ryan Buchmeier <rwbleo@gmail.com>
2017-05-26 20:38:09 -0600

    Remove artifact code

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -6,35 +6,6 @@ import { Provider, connect } from 'react-redux';
 import App from './components/AppComponent.jsx';
 import { race_reducer } from './reducers/race_reducer.js';
 
-/*
-var race_reducer = function(state, action) {
-    if (state == undefined) {
-        var data = {
-            race: {racers:[]},
-            races: []
-        };
-        return data;
-    }
-    var newState = state;
-    switch(action.type) {
-        case 'change_race':
-            newState = Object.assign({}, state, {race: action.payload});
-            break;
-        case 'load_races':
-            newState = Object.assign({}, state, {races: action.payload});
-            break;
-        case 'sort_time':
-            var results = state.race.racers;
-            results.sort(function (a, b) {
-                return a.time - b.time;
-            });
-            const newRace = Object.assign({}, state.race, {racers: results});
-            newState = Object.assign({}, state, {race: newRace});
-    }
-    return newState;
-}
-*/
-
 const store = createStore(race_reducer);
 
 function mapStateToProps(state) {
