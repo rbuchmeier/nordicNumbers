184348bf4df4f74abe51eae3094dee653bb28445
Ryan Buchmeier <rwbleo@gmail.com>
2017-06-15 16:44:44 -0600

    Added Place column to Race View

--- src/client/app/components/ViewComponent.jsx
+++ src/client/app/components/ViewComponent.jsx
@@ -7,18 +7,20 @@ var ViewComponent = React.createClass({
       <div className='rounded col-md-9' id='view-component'>
         <h3>Race View</h3>
       <div className="row" key="label"><em>
+            <div className="col-md-2">Place</div>
             <div className="col-md-3">Name</div>
-            <div className="col-md-3">Grade</div>
+            <div className="col-md-2">Grade</div>
             <div className="col-md-3">Time</div>
-            <div className="col-md-3">Team</div>
+            <div className="col-md-2">Team</div>
       </em></div>
-            {this.props.race.racers.map(function(racer) {
+            {this.props.race.racers.map(function(racer, i) {
                 return (
                     <div className="row" key={racer.first + racer.last + racer.time}>
+                   <div className="col-md-2"> {i+1}</div>
                    <div className="col-md-3"> {racer.first} {racer.last}</div>
-		   <div className="col-md-3">{racer.grade}</div>
+		   <div className="col-md-2">{racer.grade}</div>
                    <div className="col-md-3">{moment('2017-01-01').startOf('day').seconds(racer.time).format('H:mm:ss')}</div>
-                   <div className="col-md-3">{racer.team}</div>
+                   <div className="col-md-2">{racer.team}</div>
                     </div>
                 );
             })}
