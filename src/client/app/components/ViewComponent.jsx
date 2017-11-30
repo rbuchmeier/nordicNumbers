a45b8bb2a0bab0db5231376ff916b10e351ece54
Ben Blue Groneman <bgroneman@localhost.localdomain>
2017-11-30 13:04:58 -0700

    fixed classname issue

--- src/client/app/components/ViewComponent.jsx
+++ src/client/app/components/ViewComponent.jsx
@@ -7,22 +7,23 @@ class ViewComponent extends React.Component{
       <div className='rounded col-md-9' id='view-component'>
         <h3>race view</h3>
       <div className="row" key="label"><em>
-            <div className="col-md-2">place</div>
-            <div className="col-md-3">name</div>
-            <div className="col-md-1">grade</div>
-            <div className="col-md-2">time</div>
-            <div className="col-md-2">team</div>
+            <div className="col-md-2">Location</div>
+            <div className="col-md-3">Name</div>
+            <div className="col-md-1">Grade</div>
+            <div className="col-md-2">Time</div>
+            <div className="col-md-2">Team</div>
             <div className="col-md-2">% Back</div>
       </em></div>
             {this.props.race.racers.map(function(racer, i) {
                 return (
-                    <div className="row" key={racers.first + racers.last + racers.time}>
-                   <div className="col-md-2"> {i+1}</div>
-                   <div className="col-md-3"> {racers.first} {racers.last}</div>
-		   <div className="col-md-1">{racers.grade}</div>
-                   <div className="col-md-2">{moment('2017-01-01').startOf('day').seconds(racer.time).format('H:mm:ss')}</div>
-                   <div className="col-md-2">{racers.team}</div>
-                   <div className="col-md-2">{racers.percent_back} %</div>
+                    <div className="row" key={racer.first + racers.last + racers.time}>
+                      <div className="col-md-2"> {i+1}</div>
+                      <div className="col-md-1">{racer.location}</div>
+                      <div className="col-md-2"> {racer.first} {racers.last}</div>
+		                  <div className="col-md-1">{racer.grade}</div>
+                      <div className="col-md-2">{moment('2017-01-01').startOf('day').seconds(racer.time).format('H:mm:ss')}</div>
+                      <div className="col-md-2">{racer.level}</div>
+                      <div className="col-md-2">{racer.percent_back} %</div>
                     </div>
                 );
             })}
