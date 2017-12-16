aa0580cdc545cf1a76af16956c2ace75c9c580c3
benBG <ben@wyominginc.com>
2017-12-15 20:52:51 -0700

    fixed package.json

---
+++ src/client/app/models/RaceModel.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let Sequelize = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    const raceModel = sequelize.define('races', {
+        location: {
+            type: Sequelize.STRING(10)
+        },
+        date: {
+            type: Sequelize.DATEONLY
+        },
+        class: {
+            type: Sequelize.STRING(10)
+        },
+        level: {
+            type: Sequelize.STRING(3)
+        },
+        distance: {
+            type: Sequelize.STRING(3)
+        },
+        gender: {
+            type: Sequelize.STRING(9)
+        }
+    }, {
+        freezeTableName: true // Model tableName will be the same as the model name
+    });
+
+    return raceModel
+
+/*
+    // Here for documentation
+    raceModel.sync({}).then(function () {
+        // Table created
+        return raceModel.create({
+            location: 'casper'
+        });
+    });
+*/
+
+    return raceModel;
+};
